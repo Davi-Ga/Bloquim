421b637fb4e74e6c00f4f2a77fb203b79c30c712
Davi-Ga <davigaldinoky@gmail.com>
2021-12-14 16:22:35 -0300

    Adicionei a tela e controller de anotacões

---
+++ Bloquim/src/anotacaoController.java
@@ -0,0 +1,43 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class anotacaoController {
+
+    @FXML
+    private Button Anotação;
+
+    @FXML
+    private Button addAnotacao;
+
+    @FXML
+    private Text nomeCaderno;
+
+    @FXML
+    private Button voltarBtn;
+
+    @FXML
+    private Button xBtn;
+
+    @FXML
+    void AdicionaAnotacao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fechaAba(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verAnotação(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltaTela(ActionEvent event) {
+
+    }
+
+}
