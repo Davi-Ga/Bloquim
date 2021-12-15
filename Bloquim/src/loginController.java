72b9036ad6617db5f1b186bbfdf030dcf62d403f
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-14 21:54:13 -0300

    Agora se faz login com uma senha e um usar pré-definidos, salvos em variáveis

--- Bloquim/src/loginController.java
+++ Bloquim/src/loginController.java
@@ -7,11 +7,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 
 
 public class loginController {
+
+    private String nomeUsuarioStringDeComparacao = "batata";
+    private String senhaUsuarioStringDeComparacao = "batata123";
+    
 
     @FXML
     private Button cadastreSeBtn;
@@ -32,8 +37,43 @@ public class loginController {
     private Button xBtn;
 
     @FXML
-    void entrarApp(ActionEvent event) {
+    void entrarApp(ActionEvent event) throws IOException  {
+        if(!usuarioTextBox.getText().equals(nomeUsuarioStringDeComparacao)){
+            System.out.println("O nome de usuário digitado é inválido");
+            
+        }
+        else if(!senhaTextBox.getText().equals(senhaUsuarioStringDeComparacao)){
+            System.out.println("A senha digitada é inválida");
+        }
+        else{
 
+
+            Parent root;
+            try {
+                root = FXMLLoader.load(getClass().getResource("/FXML/telaAnotacao.fxml"));
+                Stage stage = new Stage();
+                stage.setTitle("My New Stage Title");
+                stage.setScene(new Scene(root));
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.show();
+                // Hide this current window (if this is what you want)
+                //((Node)(event.getSource())).getScene().getWindow().hide();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            // //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/telaAnotacao.fxml"));
+            // Parent telaNova = FXMLLoader.load(getClass().getResource("/FXML/telaAnotacoes.fxml"));
+            // // Stage anotacoeStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            // Scene anotacoesScene = new Scene(telaNova);
+            
+            
+            // // anotacoeStage.setScene((anotacoesScene));
+            // // anotacoeStage.show();
+            
+        }
     }
 
     @FXML
@@ -57,12 +97,13 @@ public class loginController {
     }
 
     @FXML
-    void vaiTelaCadastro(ActionEvent event) throws IOException {
+    void vaiTelaCadastro(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/telacadastro.fxml"));
         Stage cadastroStage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene cadastroscene = new Scene(root);
         cadastroStage.setScene(cadastroscene);
         cadastroStage.show();
     }
+    
 
 }
