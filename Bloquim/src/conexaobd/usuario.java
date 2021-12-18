0d53a992085cb758810f72875496212152f04601
Davi-Ga <davigaldinoky@gmail.com>
2021-12-18 17:16:16 -0300

    Crição da classe DAO

--- Bloquim/src/conexaobd/usuario.java
+++ Bloquim/src/conexaobd/usuario.java
@@ -1,5 +1,36 @@
 package conexaobd;
 
 public class usuario {
-    
-}
+    private String nome;
+    private String senha;
+    private String email;
+
+        public usuario(String nome, String senha, String email){
+            this.nome = nome;
+            this.senha = senha;
+            this.email = email;
+        } 
+
+        public String getnome(){
+            return nome;
+        }
+        public void setnome (String nome){
+            this.nome = nome;
+        }
+
+
+        public String getsenha(){
+            return senha;
+        }
+        public void setsenha(String senha){
+            this.senha = senha;
+        }
+
+
+        public String getemail(){
+            return senha;
+        }
+        public void setemail(String email){
+            this.email = email;
+        }
+}      
--- Bloquim/src/conexaobd/usuarioDAO.java
+++ Bloquim/src/conexaobd/usuarioDAO.java
@@ -1,5 +1,24 @@
 package conexaobd;
 
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class usuarioDAO {
+
+    private final Connection conexao;
+
+    public usuarioDAO(Connection conexao){
+        this.conexao = conexao;
+    }
+
+    public void inserir(usuario usuariol){
     
+            
+        String sql = "INSERT INTO login(usuario,senha,email) VALUES ('"+usuariol.getnome()+"','"+usuariol.getsenha()+"','"+usuariol.getemail()+"');";
+        PreparedStatement stnt= conexao.prepareStatement(sql);
+        stnt.execute();
+        conexao.close();
+    }
 }
+,
