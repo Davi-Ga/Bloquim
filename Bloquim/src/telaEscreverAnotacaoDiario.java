5f05f243dd84db5fee5d2c0e99942e5c27e27718
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-01-10 20:05:10 -0300

    .

--- Bloquim/src/telaEscreverAnotacaoDiario.java
+++ Bloquim/src/telaEscreverAnotacaoDiario.java
@@ -1,10 +1,20 @@
+import java.io.IOException;
+
+import application.sceneController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class telaEscreverAnotacaoDiario {
+
+    @FXML
+    public void initialize() {
+        // textoAnotacao.setMaxWidth(200);
+    }
 
     @FXML
     private ChoiceBox<?> humorDia;
@@ -33,12 +43,21 @@ public class telaEscreverAnotacaoDiario {
 
     @FXML
     void fechaAba(ActionEvent event) {
-
+        fecharStage();
     }
 
     @FXML
-    void voltaTela(ActionEvent event) {
-
+    void voltaTela(ActionEvent event) throws IOException {
+        mostrarTelaDiario(event);
     }
 
+    void fecharStage(){
+        Stage stage=(Stage)this.xBtn.getScene().getWindow();
+        stage.close();
+    }
+    void mostrarTelaDiario(ActionEvent event) throws IOException{
+        fecharStage();
+        sceneController sc = new sceneController();
+        sc.trocarParaTelaDiario(event);
+    }
 }
