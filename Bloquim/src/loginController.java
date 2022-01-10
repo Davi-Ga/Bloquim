d122fb71b488810a3ef5fc76dc08001f168bfc31
Davi-Ga <davigaldinoky@gmail.com>
2022-01-10 19:34:19 -0300

    Ajustes

--- Bloquim/src/loginController.java
+++ Bloquim/src/loginController.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-
 import application.sceneController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,7 +11,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-
 import javafx.scene.Node;
 import javafx.scene.Scene;
 
