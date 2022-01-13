3080b419a01424330e4407bc07fe94dd8245f785
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-01-13 17:28:40 -0300

    Correção da inversão nos parâmetros da void "intiData"

--- Bloquim/src/FXMLsControllers/anotacaoController.java
+++ Bloquim/src/FXMLsControllers/anotacaoController.java
@@ -15,11 +15,36 @@ import javafx.stage.Stage;
 public class anotacaoController {
     private String nome;
     private String email;
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+
+
+
     
     @FXML
-    public void initData(String nome, String email){
-        this.nome= nome;
-        this.email=email;
+    public void initData(String email, String nome){
+        setEmail(email);
+        setNome(nome);
     }
 
     @FXML
