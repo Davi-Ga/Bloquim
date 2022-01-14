e4e36cfa560b9a9f7401dcc7220a4439663257f1
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-01-14 16:45:09 -0300

    .

--- Bloquim/src/FXMLsControllers/loginController.java
+++ Bloquim/src/FXMLsControllers/loginController.java
@@ -1,5 +1,5 @@
 package FXMLsControllers;
-
+import validadorDeTexto.validadorDeTexto;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -118,7 +118,8 @@ public class loginController {
                 sceneController sc = new sceneController();
                 Stage stage=(Stage)xBtn.getScene().getWindow();
                 stage.close();
-                sc.trocarParaTelaCaderno(event,usuarioAutenticar.getemail(),usuarioAutenticar.getnome());    
+                
+                sc.trocarParaTelaCaderno(event,usuarioAutenticar.getemail(),name);    
                 if(usuarioAutenticar.getnome().isEmpty() || usuarioAutenticar.getnome()==null){
                     System.out.println("A variável 'nome' está nula!");
                 }
