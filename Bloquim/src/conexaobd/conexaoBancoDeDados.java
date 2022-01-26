3b7d48758e27017dbdec8b1443dfb605cf4e282e
Davi-Ga <davigaldinoky@gmail.com>
2022-01-26 09:38:44 -0300

    Alteração do banco

--- Bloquim/src/conexaobd/conexaoBancoDeDados.java
+++ Bloquim/src/conexaobd/conexaoBancoDeDados.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class conexaoBancoDeDados{
 
     public Connection getConnection() throws SQLException{
-        Connection conexaoBancoDeDados = DriverManager.getConnection("jdbc:postgresql://db-bloquim.postgres.uhserver.com:5432/db_bloquim","adminbloquim","kyntra93791@");
+        Connection conexaoBancoDeDados = DriverManager.getConnection("jdbc:postgresql://db-bloquim.postgres.uhserver.com:5432/db_bloquim","adminbloquim","kyntra93791");
         return conexaoBancoDeDados;
     }
 }
