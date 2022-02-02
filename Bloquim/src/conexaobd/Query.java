1d1c21063d049f75b0948a8c798d991b154353ce
Davi-Ga <davigaldinoky@gmail.com>
2022-02-02 18:29:23 -0300

    Busca cadernos

--- Bloquim/src/conexaobd/Query.java
+++ Bloquim/src/conexaobd/Query.java
@@ -2,17 +2,21 @@ package conexaobd;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class Query {
 
     public static void BuscaCadernos(Integer id_usuario) throws SQLException{
         Connection conexao = new conexaoBancoDeDados().getConnection();
-
-        String sql = "SELECT id_caderno,nome FROM caderno as c JOIN login as l ON c.id_usuario=l.id_usuariofk";
+        String sql = "SELECT id_caderno,nome FROM caderno WHERE id_usuariofk = ?";
         PreparedStatement stnt = conexao.prepareStatement(sql);
+        stnt.setInt(1, id_usuario);
         stnt.execute();
+        ResultSet resultSet = stnt.getResultSet();
+        resultSet.next();
     }
 
 }
