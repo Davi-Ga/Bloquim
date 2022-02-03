69e198d49b4089158a646acd59aec38653f7e207
Davi-Ga <davigaldinoky@gmail.com>
2022-02-02 21:16:29 -0300

    Tela AddCaderno

--- Bloquim/src/FXMLsControllers/sistemalogin/cadernos/telaAddCaderno.java
+++ Bloquim/src/FXMLsControllers/sistemalogin/cadernos/telaAddCaderno.java
@@ -1,10 +1,16 @@
 package FXMLsControllers.sistemalogin.cadernos;
+import java.sql.SQLException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class telaAddCaderno {
+
+    protected int id;
+    protected String nome;
 
     @FXML
     private Button cancelaBtn;
@@ -15,14 +21,37 @@ public class telaAddCaderno {
     @FXML
     private TextField nomeCadernoText;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @FXML
-    void cancelaCriarCaderno(ActionEvent event) {
+    void cancelaCriarCaderno(ActionEvent event) throws SQLException {
+        fecharStage();
 
     }
 
     @FXML
-    void criaCaderno(ActionEvent event) {
-        
+    void criaCaderno(ActionEvent event) throws SQLException {
+        String nome = nomeCadernoText.getText();
+        conexaobd.Query.insereCadernos(nome, getId());
+    }
+
+    private void fecharStage(){
+        Stage stage = (Stage) cancelaBtn.getScene().getWindow();
+        stage.close();
     }
 
 }
